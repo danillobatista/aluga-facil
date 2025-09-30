a890a6161283d6303b91939541377aac06873c54
Danillo Batista <danillobatista@gmail.com>
2025-09-30 13:57:48 -0300

    initial commit - added files via upload

---
+++ alugafacil/src/test/java/com/example/alugafacil/alugafacil/AlugaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alugafacil.alugafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
